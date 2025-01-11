7c14a336919b72716e4c0903e825d0e031cc49e5
junkmd <jun.k.job.finding12@gmail.com>
2025-01-11 23:35:05 +0900

    Add `__version__`.

--- olinguito/__init__.py
+++ olinguito/__init__.py
@@ -1,5 +1,7 @@
 """A Python function decorator generating JSON schema based on signatures."""
 
+__version__ = "0.1.0"
+
 from .mapping import Mapping  # noqa
 from .schema import description  # noqa
 from .wrapping import Wrapper, wrap  # noqa
